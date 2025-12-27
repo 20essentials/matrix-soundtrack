9a2805b94bf0fd12398c2dce6ce630db8db2e395
DominicDev <s0l0estudi4r@gmail.com>
2025-12-27 10:43:03 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'MATRIX';
+export let nameSongs = [
+	"CLUBBED TO DEATH ", /*1*/
+	"FURIOUS ANGELS", /*2*/
+	"WAKE UP", /*3*/
+	"SPYBREAK! ", /*4*/
+	"ROCK IS DEAD", /*5*/
+	"DRAGULA ", /*6*/
+	"PRODIGY", /*7*/
+	"DU HAST - RAMMSTEIN", /*8*/
+	"PRIME AUDIO SOUP", /*9*/
+	" MY OWN SUMMER", /*10*/
+	"LEAVE YOU FAR BEHIND", /*11*/
+	"BAD BLOOD", /*12*/
+	"THIS IS YOUR LIFE", /*13*/
+	"ULTRA ", /*14*/
+	"LOOK TO YOUR ORB", /*15*/
+	"MONSTER SKIN", /*16*/
+	"THE EYES OF TRUTH", /*17*/
+	" MORE HUMAN THAN HUMAN ", /*18*/
+	" DISSOLVED GIRL", /*19*/
+	"THE LIGHT", /*20*/
+]
